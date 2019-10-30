c8aaed9fcf5b5320748da772cfd0ce49eda3f049
anloffredo <loffredo.ap@gmail.com>
2019-10-29 23:05:15 -0700

    Auto Commit

---
+++ Exceptions/MissingConfigurationException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Penguin.Configuration.Abstractions.Exceptions
+{
+    public class MissingConfigurationException : Exception
+    {
+        public string ConfigurationName { get; set; }
+
+        public MissingConfigurationException(string configurationName, string message) : base(message)
+        {
+            ConfigurationName = configurationName;
+        }
+
+        public MissingConfigurationException(string configurationName, string message, Exception innerException) : base(message, innerException)
+        {
+            ConfigurationName = configurationName;
+        }
+    }
+}
